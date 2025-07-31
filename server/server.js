06b529f08d050ea1fdd3f3cf38c28536fc2ab8d0
Jayesh <jayashpal5@gmail.com>
2025-07-31 14:25:14 +0530

    seprate app.js and server.js done, cors done, helmet done, custom middleware 404 not found done, cookie parser done

--- server/server.js
+++ server/server.js
@@ -1,23 +1,13 @@
-
-const express = require('express');
-const errorHandler = require('./middlewares/errorHandler');
-const dbConnection = require('./config/dbConnection');
-const router = require('./routes/userRoutes');
-const { globalLimiter } = require('./utils/rateLimiters');
 require('dotenv').config();
+const app = require('./app');
+const dbConnection = require('./config/dbConnection');
 
+const PORT = process.env.PORT || 5000;
 
-const app = express(); 
+// Connect DB
+dbConnection();
 
-app.use(globalLimiter); 
-app.use(express.json()); 
-
-const PORT = process.env.PORT
-dbConnection(); 
-
-app.use('/users', router)
-
-app.use(errorHandler); 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at ${PORT}`);   
-})
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
